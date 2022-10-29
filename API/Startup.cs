b983e02f86b10f9b45ef9df3b54f75e054b61b20
SamirKabashi <sk52317@ubt-uni.net>
2022-10-29 11:17:03 +0200

    PerditsimiUpdatave

--- API/Startup.cs
+++ API/Startup.cs
@@ -43,8 +43,6 @@ namespace API
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
             }
 
-            // app.UseHttpsRedirection();
-
             app.UseRouting();
 
             app.UseCors("CorsPolicy");
