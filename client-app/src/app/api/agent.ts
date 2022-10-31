7243244d1acbe34341db12e0633567c0dbd278be
SamirKabashi <sk52317@ubt-uni.net>
2022-10-31 23:15:02 +0100

    Perditsim

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,13 +1,16 @@
 import axios, { AxiosResponse } from 'axios';
 import { Activity } from '../models/activity';
 
-const sleep = (delay: number) => {
-    return new Promise((resolve) => {
+const sleep = (delay: number) =>{
+    return new Promise((resolve) =>{
         setTimeout(resolve, delay)
     })
 }
-axios.interceptors.response.use(async response => {
-    try {
+
+axios.defaults.baseURL = 'http://localhost:5000/api';
+
+axios.interceptors.response.use(async response =>{
+   try {
         await sleep(1000);
         return response;
     } catch (error) {
@@ -16,23 +19,25 @@ axios.interceptors.response.use(async response => {
     }
 })
 
-const responseBody = <T> (response: AxiosResponse<T>) => response.data;
+const responseBody = <T>(response: AxiosResponse<T>) => response.data;
 
-const requests = {
+const requests ={
     get: <T> (url: string) => axios.get<T>(url).then(responseBody),
     post: <T> (url: string, body: {}) => axios.post<T>(url, body).then(responseBody),
-    put: <T> (url: string, body: {}) => axios.put<T>(url, body).then(responseBody),
+    put: <T> (url: string, body:{}) => axios.put<T>(url, body).then(responseBody),
     del: <T> (url: string) => axios.delete<T>(url).then(responseBody),
 }
 
 const Activities = {
     list: () => requests.get<Activity[]>('/activities'),
-    details: (id: string) => requests.get<Activity>(`/activities/${id}`),
-    create: (activity: Activity) => axios.post<void>('/activities', activity),
-    update: (activity: Activity) => axios.put<void>(`/activities/${activity.id}`, activity),
+    details:(id: string) => requests.get<Activity>(`/activities/${id}`),
+    create: (activity: Activity) => axios.post('activities', activity),
+    update: (activity: Activity) => axios.put(`/activities/${activity.id}`, activity),
     delete: (id: string) => axios.delete<void>(`/activities/${id}`)
 }
+
 const agent = {
     Activities
 }
+
 export default agent;
